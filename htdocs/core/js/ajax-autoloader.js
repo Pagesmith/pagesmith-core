552312bac1c341ec7317ee336036d5a1ab6673db
James Smith <js5@sanger.ac.uk>
2012-07-24 13:49:05 +0000

    new core structure

--- htdocs/core/js/ajax-autoloader.js
+++
@@ -1,9 +0,0 @@
-/*----------------------------------------------------------------------
-  Ajax autloader functionality
-------------------------------------------------------------------------
-  Dependency: jquery.livequery.js, base.js
-------------------------------------------------------------------------
-  For each class="ajax" entry add code which takes the title tag
-  and retrieves the appropriate URL and replaces the Div with it
-----------------------------------------------------------------------*/
-
