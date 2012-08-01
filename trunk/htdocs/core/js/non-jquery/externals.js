e9a97af72a61e159e63d2b544e4db8944920e760
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2012-08-01 14:01:25 +0000

    fixed externals to work without rel

--- trunk/htdocs/core/js/non-jquery/externals.js
+++ trunk/htdocs/core/js/non-jquery/externals.js
@@ -1,8 +1,12 @@
-var x = document.getElementsByTagName('A'), i, t;
+var x = document.getElementsByTagName('A'), i, t, m = document.location.href.match( /https?:\/\/([^\/]+)/ ), domain = '';
+if( m ) {
+  domain = m[1];
+}
 for (i = x.length; i; i) {
   i--;
   t = x[i];
-  if (t.rel === 'external') {
+  m = t.href.match( /^https?:\/\/([^\/]+)/ );
+  if( m && m[1]!==domain || t.rel === 'external'  ) {
     t.target = '_blank';
     t.title = '* This link opens in a new window to ' + t.href;
   }
