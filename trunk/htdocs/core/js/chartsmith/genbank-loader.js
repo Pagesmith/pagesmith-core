679c35abb0fb7df9043d38ba76862d9dd2d323ce
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2013-02-06 09:34:19 +0000

    moved genbank code to core

---
+++ trunk/htdocs/core/js/chartsmith/genbank-loader.js
@@ -0,0 +1,14 @@
+/*globals Chartsmith: true */
+
+function render_genbank(dom_node_ref) {
+  var dom_node = $(dom_node_ref), url = dom_node.attr('title'), dom_node_width = $(dom_node).width();
+  dom_node.removeClass('genbank').removeAttr('title');
+  $.getJSON(url, {}, function (resp) {
+    //var genbank = new Chartsmith.Genbank(dom_node, resp, dom_node_width);
+    (new Chartsmith.Genbank(dom_node, resp, dom_node_width))  // Create the object
+      .filter_features().process_features()                   // Prep features!
+      .place_features().resize_image().render_features();     // These can be used to re-draw the image!
+  });
+}
+
+$('.genbank:visible').livequery(function () { render_genbank(this); });
