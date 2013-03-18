cbbfaf1ff7977d020b69c077f1bfbee9156b637c
James Smith <js5@sanger.ac.uk>
2013-03-18 13:07:15 +0000

    pull out expandy/collapsy tree nav

---
+++ htdocs/core/js/beta/nav.js
@@ -0,0 +1,7 @@
+$('.branch span').on( 'click', function(){ $(this).parent().toggleClass('coll');return false; } );
+
+$(document).ready(function() {
+    // to show it in an alert window
+  var link_node = $('a[href="'+window.location.pathname.split(/\//).pop()+'"]');
+  link_node.parents('.coll').toggleClass('coll');
+});
