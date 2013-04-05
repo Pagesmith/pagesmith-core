98b36a02ec349106b683678c2c09bad2dba756fc
James Smith <js5@sanger.ac.uk>
2013-04-05 15:01:12 +0000

    Readonly -> Const

--- lib/Pagesmith/Core.pm
+++ lib/Pagesmith/Core.pm
@@ -15,8 +15,8 @@ use utf8;
 
 use version qw(qv); our $VERSION = qv('0.1.0');
 
-use Readonly qw(Readonly);
-Readonly my $WIDTH => 78;
+use Const::Fast qw(const);
+const my $WIDTH => 78;
 
 use base qw(Exporter);
 
