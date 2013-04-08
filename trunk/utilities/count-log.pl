a619f0885a67c95ce1510cbc238d120b48e41253
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2013-04-08 14:50:47 +0000

    updated script

--- trunk/utilities/count-log.pl
+++ trunk/utilities/count-log.pl
@@ -23,8 +23,10 @@ use Cwd qw(abs_path);
 use POSIX qw(strftime floor);
 
 use Const::Fast qw(const);
-const my $MINUTE   => 60; ## 10 minutes...
+const my $MINUTE   => 60;
 const my $INTERVAL => 5 * $MINUTE; ## 10 minutes...
+const my $MAXDUR   => 3;
+const my $TOTDUR   => 4;
 
 my $path    = dirname(dirname(abs_path($PROGRAM_NAME)));
 my $system  = basename($path);
@@ -39,8 +41,9 @@ my $events;
 if( open my $fh, q(<), $al ) {
   while(<$fh>) {
     if( m{"[ ](\d+[.]\d+)/(\d+[.]\d+)[ ]\[}mxs ) {
-      $events->{$1}{'s'}++;
-      $events->{$2}{'e'}++;
+      my $dur = $2 - $1;
+      push @{$events->{$1}{'s'}}, $dur;
+      push @{$events->{$2}{'e'}}, $dur;
     }
   }
   close $fh; ## no critic (RequireChecked)
@@ -49,11 +52,13 @@ if( open my $fh, q(<), $al ) {
 my @event_times = sort { $a <=> $b } keys %{$events};
 my %intervals;
 my $n_req   = 0;
+my $max_dur = 0;
 my $max_req = 0;
 my $max_time = 0;
+my $max_dur_time = 0;
 foreach (@event_times) {
-  my $st = $events->{$_}{'s'}||0;
-  my $fi = $events->{$_}{'e'}||0;
+  my $st = exists $events->{$_}{'s'} ? scalar @{$events->{$_}{'s'}} : 0;
+  my $fi = exists $events->{$_}{'e'} ? scalar @{$events->{$_}{'e'}} : 0;
   $n_req += $st - $fi;
   my @time = localtime $_;
   my $s_past_hour = $time[0] + $MINUTE * $time[1];
@@ -61,6 +66,7 @@ foreach (@event_times) {
   $time[0] = $s_block % $MINUTE;
   $time[1] = ($s_block - $time[0])/$MINUTE;
   my $time = strftime( q(%Y-%m-%d %H:%M:%S), @time );
+
   if( $n_req > $max_req ) {
     $max_req  = $n_req;
     $max_time = $_;
@@ -70,13 +76,22 @@ foreach (@event_times) {
     $intervals{$time}[1] += $st;
     $intervals{$time}[2] += $fi;
   } else {
-    $intervals{$time} = [ $n_req, $st, $fi ];
+    $intervals{$time} = [ $n_req, $st, $fi, 0, 0 ];
+  }
+  foreach my $dur ( @{$events->{$_}{'s'}||[]} ) {
+    $max_dur = $dur if $dur > $max_dur;
+    $intervals{$time}[$MAXDUR] =$dur if $dur > $intervals{$time}[$MAXDUR];
+    $intervals{$time}[$TOTDUR]+=$dur;
   }
 }
 
 foreach (sort keys %intervals) {
   my $diff = $intervals{$_}[1]-$intervals{$_}[2];
-  printf "%s\t%5d\t%5d\t%5d\t\t%s\n", $_, @{$intervals{$_}}, $diff ? sprintf '%s%d', $diff < 0 ? q(-):q(+), abs $diff : q();
+  printf "%s\t%5d\t%5d\t%5d\t%12.5f\t%12.5f\t\t%s\n",
+    $_,
+    @{$intervals{$_}}[0,1,2,$MAXDUR],
+    $intervals{$_}[1] ? $intervals{$_}[$TOTDUR]/$intervals{$_}[1] : 0,
+    $diff ? sprintf '%s%d', $diff < 0 ? q(-):q(+), abs $diff : q();
 }
 
 printf {*STDERR} "\nMax requests... %d \@ %s\n\n", $max_req, strftime( q(%Y-%m-%d %H:%M:%S), localtime $max_time );
