91600c78d3cf99e0e21620c93c3d8833c325c5c5
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2013-08-14 13:21:43 +0000

    move expand/collapse to core

--- trunk/htdocs/core/js/pagesmith/treenav.js
+++ trunk/htdocs/core/js/pagesmith/treenav.js
@@ -5,7 +5,3 @@ $(document).ready(function() {
   var link_node = $('a[href="'+window.location.pathname+'"]');
   link_node.parents('.coll').toggleClass('coll');
 });
-
-$('body')
-  .on('click','#main  .toggle-width',function() { $('#main').attr('id','mainx'); $('#rhs').attr('id','rhsx'); $(this).html('&gt;=&lt;');} )
-  .on('click','#mainx .toggle-width',function() { $('#mainx').attr('id','main'); $('#rhsx').attr('id','rhs'); $(this).html('&lt;=&gt;');} );
