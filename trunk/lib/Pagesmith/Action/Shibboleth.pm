376bcdeec8a2503334e6d080e90e612caf848b68
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2013-09-11 15:46:59 +0000

    add functionality to log user in

--- trunk/lib/Pagesmith/Action/Shibboleth.pm
+++ trunk/lib/Pagesmith/Action/Shibboleth.pm
@@ -15,7 +15,7 @@ use utf8;
 
 use version qw(qv); our $VERSION = qv('0.1.0');
 
-use base qw(Pagesmith::Action);
+use base qw(Pagesmith::Action::OAuth2);
 use English qw(-no_match_vars $INPUT_RECORD_SEPARATOR);
 use Const::Fast qw(const);
 
@@ -30,7 +30,33 @@ use Pagesmith::Session::User;
 
 sub run {
   my $self  = shift;
-  return $self->html->wrap('TEST','<% Developer_Information %>')->ok;
+  my $form_code = $self->next_path_info;
+  return $self->error( 'Unable to login user, erroneous URL' ) unless $form_code;
+  my $form = Pagesmith::Utils::FormObjectCreator
+    ->new( $self->r, $self->apr )
+    ->form_from_code( $form_code );
+  return $self->error( 'Unable to login user, erroneous URL' ) unless $form;
+  return $self->error( 'Unable to login user' ) unless $self->create_session;
+  return $self->redirect( $form->destroy_object->attribute('ref') );
+}
+
+sub create_session {
+  my ( $self, $user_info, $system_key ) = @_;
+  $self->r->subprocess_env();
+  $self->dumper( \%ENV );
+  return unless exists $ENV{'eppn'};
+  my $user_details = {
+    'method' => 'Shibboleth::'.$ENV{'Shib-Identity-Provider'},
+    'id'     => exists $ENV{'uid'} ? $ENV{'uid'} : $ENV{'eppn'},
+    'email'  => $ENV{'eppn'},
+    'name'   => exists $ENV{'cn'}                             ? $ENV{'cn'}
+              : exists $ENV{'displayName'}                    ? $ENV{'displayName'}
+              : exists $ENV{'sn'} && exists $ENV{'givenName'} ? "$ENV{'givenName'} $ENV{'sn'}"
+              : $ENV{'eppn'},
+  };
+  $self->dumper( $user_details );
+  Pagesmith::Session::User->new( $self->r )->initialize($user_details)->store->write_cookie if $user_details->{'email'};
+  return 1;
 }
 
 1;
