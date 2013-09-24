c8c5e73be3aa524abc3c0774c3f58354987774e1
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2013-09-24 10:24:30 +0000

    added date code

--- trunk/lib/Pagesmith/Cache/Base.pm
+++ trunk/lib/Pagesmith/Cache/Base.pm
@@ -17,6 +17,27 @@ use version qw(qv); our $VERSION = qv('0.1.0');
 
 use Const::Fast qw(const);
 const my $FUTURE => 0x7fffffff;
+const my %UNIT_MULTIPLIER => (
+  's'       =>          1,
+  'sec'     =>          1,
+  'second'  =>          1,
+  'm'       =>         60,
+  'min'     =>         60,
+  'minute'  =>         60,
+  'h'       =>      3_600,
+  'hr'      =>      3_600,
+  'hour'    =>      3_600,
+  'd'       =>     86_400,
+  'day'     =>     86_400,
+  'w'       =>    604_800,
+  'wk'      =>    604_800,
+  'week'    =>    604_800,
+  'mn'      =>  2_678_400,
+  'month'   =>  2_678_400,
+  'y'       => 31_622_400,
+  'yr'      => 31_622_400,
+  'year'    => 31_622_400,
+);
 
 use base qw(Exporter);
 
@@ -24,19 +45,19 @@ our @EXPORT_OK = qw(expires columns);
 our %EXPORT_TAGS = ( 'ALL' => \@EXPORT_OK );
 
 my $columns = {
-  'tmpfile'   => [qw(type filename)],
   'action'    => [qw(type cachekey)],
+  'appdata'   => [qw(app cachekey)],
   'component' => [qw(type cachekey)],
-  'variable'  => [qw(type cachekey)],
-  'feed'      => [qw(url)],
   'config'    => [qw(location filename)],
-  'tmpdata'   => [qw(type cachekey)],
+  'feed'      => [qw(url)],
   'form'      => [qw(cachekey)],
   'form_file' => [qw(cachekey filekey file_ndx)],
-  'template'  => [qw(type)],
   'page'      => [qw(type uri params)],
-  'appdata'   => [qw(app cachekey)],
   'session'   => [qw(type session_key)],
+  'template'  => [qw(type)],
+  'tmpdata'   => [qw(type cachekey)],
+  'tmpfile'   => [qw(type filename)],
+  'variable'  => [qw(type cachekey)],
 };
 
 sub columns {
@@ -46,8 +67,14 @@ sub columns {
 
 sub expires {
   my $expires = shift;
-
   $expires ||= 0;
+  if( $expires =~ m{\s}mxs ) {
+    my ($count, $unit) = split m{\s+}mxs, $expires;
+    $unit ||= 'day';
+    $unit = lc $unit;
+    $unit =~ s{s\Z}{}mxs;
+    $expires = - $count * ( exists $UNIT_MULTIPLIER{ $unit } ? $UNIT_MULTIPLIER{ $unit } : 1 );
+  }
   return
       $expires < 0 ? time - $expires
     : $expires > 0 ? $expires
