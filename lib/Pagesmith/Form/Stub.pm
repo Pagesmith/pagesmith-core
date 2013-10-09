bdbff36979718aee58f3ec77203869698757452f
James Smith <js5@sanger.ac.uk>
2013-10-09 11:24:04 +0000

    push make_simple/bake onto form stub

--- lib/Pagesmith/Form/Stub.pm
+++ lib/Pagesmith/Form/Stub.pm
@@ -44,4 +44,29 @@ sub overide_url {
   my $self = shift;
   return $self->{'submit_url'};
 }
+
+sub make_simple {
+  my $self = shift;
+  ## no critic (LongChainsOfMethodCalls)
+  $self
+    ->add_class(  'form',     'check'          )
+    ->add_class(  'form',     'cancel_quietly' )
+    ->set_option( 'no_reset'                   )
+    ->set_option( 'cancel_button',   0         )
+    ->set_option( 'required_string', q()       )
+    ->set_option( 'optional_string', q()       )
+    ->make_form_get
+    ->set_option( 'do_not_pass_ref' )
+    ->add_stage('input')->set_next('Go');
+  ## use critic
+  return $self;
+}
+
+sub bake {
+  my $self = shift;
+  $self->update_from_apr->add_confirmation_stage;
+  return $self;
+}
+
 1;
+
