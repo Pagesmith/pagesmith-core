e37b435dba9f669bb8fb3433369515cbaa2802e1
James Smith <js5@sanger.ac.uk>
2013-11-20 09:14:09 +0000

    just testing

--- htdocs/core/js/developer/toggle.js
+++ htdocs/core/js/developer/toggle.js
@@ -1,4 +1,2 @@
 $('body').prepend('<span class="dev-toggle devpanel">hide dev</span><span class="dev-toggle devpanel" style="display:none">show dev</span>');
-$('.dev-toggle').on('click', function () {
-  $('.devpanel').toggle();
-});
+$('.dev-toggle').on('click', function () { $('.devpanel').toggle(); });
