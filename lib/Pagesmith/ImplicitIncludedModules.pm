93ef87908e948d589ae620a48243429f817286ba
James Smith <js5@sanger.ac.uk>
2013-11-29 06:16:59 +0000

    added a module which is picked up by the boot strapper to include these additional modules

---
+++ lib/Pagesmith/ImplicitIncludedModules.pm
@@ -0,0 +1,30 @@
+package Pagesmith::ImplicitIncludedModules;
+
+## Not really used - but useful for the boot-strapper to pickup modules
+## which we don't necessarily use and won't be automagically installed
+## Author         : js5
+## Maintainer     : js5
+## Created        : 2009-08-12
+## Last commit by : $Author$
+## Last modified  : $Date$
+## Revision       : $Revision$
+## Repository URL : $HeadURL$
+
+## Supplies wrapper functions for DBI
+
+use strict;
+use warnings;
+use utf8;
+
+use version qw(qv); our $VERSION = qv('0.1.0');
+
+use DBD::Oracle;
+use DBD::mysql;
+use Perl::Critic::Bangs;
+use Perl::Critic::Itch;
+use Perl::Critic::PetPeeves::JTRAMMELL;
+use Perl::Critic::Pulp;
+use Perl::Critic::Swift;
+use Perl::Critic::StricterSubs;
+use Perl::Critic::Storable;
+1;
