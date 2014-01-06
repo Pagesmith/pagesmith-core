a97412b30d0fdc79bac51bc94320f4dbdda0b6f5
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2014-01-06 14:20:11 +0000

    updated analyse errors to use run3

--- trunk/utilities/analyse-errors.pl
+++ trunk/utilities/analyse-errors.pl
@@ -31,42 +31,47 @@ BEGIN { $ROOT_PATH = dirname(dirname(abs_path($PROGRAM_NAME))); }
 use lib "$ROOT_PATH/lib";
 
 
-use Pagesmith::Utils::Core;
+use Pagesmith::Root;
 
-my $support = Pagesmith::Utils::Core->new();
+my $s = Pagesmith::Root->new;
 
 my $lr = $ROOT_PATH.'/logs';
 ## If it is www-.... then look for
 $lr = "/www/tmp/$1/logs" if $ROOT_PATH =~ m{^\/www\/((\w+\/)?www-\w+)}mxs;
-my $al = "$lr/diagnostic.log";
 my $el = "$lr/error.log";
 
-my $command_warn  = "grep warn] $el | sort | uniq -c | sort -n";
-my $command_error = "grep error] $el | sort | uniq -c";
+my $command_warn  = ['grep', 'warn]',  $el];
+my $command_error = ['grep', 'error]', $el];
 
-my @out;
-my $rv = eval {
-  @out = $support->read_from_process( $command_warn );
-};
-if( $EVAL_ERROR ) {
-  printf "Unable to run: %s\n\n%s\n\n", $command_warn, $EVAL_ERROR;
-} else {
-  printf "%s\n-----------------------------------\n  %s\n\n", $command_warn, join "\n  ", @out;
+my $res = $s->run_cmd( $command_warn );
+
+if( $res->{'success'} ) {
+  my %counts;
+  $counts{ $_ } ++ foreach @{$res->{'stdout'}};
+  printf "%s\n-----------------------------------\n  %s\n\n", "@{$command_warn}",
+    join "\n  ",
+    map  { sprintf "%7d\t%s", @{$_} }
+    sort { $a->[0] <=> $b->[0] || $a->[1] cmp $b->[1] }
+    map  { [ $counts{$_}, $_ ] }
+    keys %counts;
 }
 
-my %H;
-$rv = eval {
-  @out = $support->read_from_process( $command_error );
-  foreach( @out ) {
-    s{\[\w{3}\s.*?\]\s*}{}mxs;
-    s{\[client.*?\]s*}{}mxs;
-    $H{$_}++;
+$res = $s->run_cmd( $command_error );
+if( $res->{'success'} ) {
+  my %counts;
+  foreach (@{$res->{'stdout'}}) {
+    s{\A\[\w{3}[ ]\w{3}.*?]\s*}{}mxs;
+    s{\[client[ ].*?\]\s*}{}mxs;
+    s{\s+\Z}{}mxs;
+    s{\A\s+}{}mxs;
+    s{\s+}{ }mxsg;
+    $counts{$_}++;
   }
-};
-if( $EVAL_ERROR ) {
-  printf "Unable to run: %s\n\n%s\n\n", $command_error, $EVAL_ERROR;
-} else {
-  printf "%s\n-----------------------------------\n  %s\n\n", $command_error, join "\n  ",
-    sort keys %H;
+  printf "%s\n-----------------------------------\n  %s\n\n", "@{$command_error}",
+    join "\n  ",
+    map  { sprintf "%7d\t%s", @{$_} }
+    sort { $a->[0] <=> $b->[0] || $a->[1] cmp $b->[1] }
+    map  { [ $counts{$_}, $_ ] }
+    keys %counts;
 }
 
