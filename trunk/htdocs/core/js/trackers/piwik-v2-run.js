90aab7f5328c337ba75215b9107b09761c6e8646
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2014-02-18 09:17:00 +0000

    pushing new version of piwik

--- trunk/htdocs/core/js/trackers/piwik-v2-run.js
+++ trunk/htdocs/core/js/trackers/piwik-v2-run.js
@@ -1,5 +1,5 @@
-(function(){
-  'use strict';
-  var _paq = _paq || [];
-  _paq.push(['trackPageView'], ['enableLinkTracking'], ['setTrackerUrl','/piwik.php']);
-}());
+/* jshint ignore:start */
+var _paq = _paq || [];
+_paq.push(['trackPageView'], ['enableLinkTracking'], ['setTrackerUrl','/piwik.php']);
+/* jshint ignore:end */
+
