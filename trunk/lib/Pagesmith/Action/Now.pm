817afb38be328061576261f52862a42172237ce6
js5 <js5@a5253675-76a5-4908-8e65-9657f5a0e7f6>
2014-07-25 14:26:45 +0000

    adding in gnu lgpl boilerplate

--- trunk/lib/Pagesmith/Action/Now.pm
+++ trunk/lib/Pagesmith/Action/Now.pm
@@ -1,5 +1,24 @@
 package Pagesmith::Action::Now;
 
+#+----------------------------------------------------------------------
+#| Copyright (c) 2013, 2014 Genome Research Ltd.
+#| This file is part of the Pagesmith web framework.
+#+----------------------------------------------------------------------
+#| The Pagesmith web framework is free software: you can redistribute
+#| it and/or modify it under the terms of the GNU Lesser General Public
+#| License as published by the Free Software Foundation; either version
+#| 3 of the License, or (at your option) any later version.
+#|
+#| This program is distributed in the hope that it will be useful, but
+#| WITHOUT ANY WARRANTY; without even the implied warranty of
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#| Lesser General Public License for more details.
+#|
+#| You should have received a copy of the GNU Lesser General Public
+#| License along with this program. If not, see:
+#|     <http://www.gnu.org/licenses/>.
+#+----------------------------------------------------------------------
+
 ## Dumps the headers of the request to log file..., and returns
 ## 204 - no content so the page isn't refreshed...
 
