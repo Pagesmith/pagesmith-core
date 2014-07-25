7b9c760033cd0af9fc1f211d45dfd20f4260e420
James Smith <js5@sanger.ac.uk>
2014-07-25 14:26:45 +0000

    adding in gnu lgpl boilerplate

--- lib/Pagesmith/Utils/Mail/File.pm
+++ lib/Pagesmith/Utils/Mail/File.pm
@@ -1,5 +1,24 @@
 package Pagesmith::Utils::Mail::File;
 
+#+----------------------------------------------------------------------
+#| Copyright (c) 2010, 2012, 2013, 2014 Genome Research Ltd.
+#| This file is part of the Pagesmith web framework.
+#+----------------------------------------------------------------------
+#| The Pagesmith web framework is free software: you can redistribute
+#| it and/or modify it under the terms of the GNU Lesser General Public
+#| License as published by the Free Software Foundation; either version
+#| 3 of the License, or (at your option) any later version.
+#|
+#| This program is distributed in the hope that it will be useful, but
+#| WITHOUT ANY WARRANTY; without even the implied warranty of
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#| Lesser General Public License for more details.
+#|
+#| You should have received a copy of the GNU Lesser General Public
+#| License along with this program. If not, see:
+#|     <http://www.gnu.org/licenses/>.
+#+----------------------------------------------------------------------
+
 ## Base class to add common functionality!
 ## Author         : js5
 ## Maintainer     : js5
