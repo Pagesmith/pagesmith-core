2b5247cdfb3478c99a11c46f18d121238c6086e0
James Smith <js5@sanger.ac.uk>
2014-08-05 15:03:18 +0000

    [force]pushing js files

---
+++ htdocs/core/dalliance/js/bam.js
@@ -0,0 +1,460 @@
+/* jshint ignore:start */
+/* -*- mode: javascript; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+// 
+// Dalliance Genome Explorer
+// (c) Thomas Down 2006-2011
+//
+// bam.js: indexed binary alignments
+//
+
+"use strict";
+
+if (typeof(require) !== 'undefined') {
+    var spans = require('./spans');
+    var Range = spans.Range;
+    var union = spans.union;
+    var intersection = spans.intersection;
+
+    var bin = require('./bin');
+    var readInt = bin.readInt;
+    var readShort = bin.readShort;
+    var readByte = bin.readByte;
+    var readInt64 = bin.readInt64;
+    var readFloat = bin.readFloat;
+
+    var lh3utils = require('./lh3utils');
+    var readVob = lh3utils.readVob;
+    var unbgzf = lh3utils.unbgzf;
+    var reg2bins = lh3utils.reg2bins;
+    var Chunk = lh3utils.Chunk;
+}
+
+
+var BAM_MAGIC = 0x14d4142;
+var BAI_MAGIC = 0x1494142;
+
+var BamFlags = {
+    MULTIPLE_SEGMENTS:       0x1,
+    ALL_SEGMENTS_ALIGN:      0x2,
+    SEGMENT_UNMAPPED:        0x4,
+    NEXT_SEGMENT_UNMAPPED:   0x8,
+    REVERSE_COMPLEMENT:      0x10,
+    NEXT_REVERSE_COMPLEMENT: 0x20,
+    FIRST_SEGMENT:           0x40,
+    LAST_SEGMENT:            0x80,
+    SECONDARY_ALIGNMENT:     0x100,
+    QC_FAIL:                 0x200,
+    DUPLICATE:               0x400,
+    SUPPLEMENTARY:           0x800
+};
+
+function BamFile() {
+}
+
+function makeBam(data, bai, callback) {
+    var bam = new BamFile();
+    bam.data = data;
+    bam.bai = bai;
+
+    bam.bai.fetch(function(header) {   // Do we really need to fetch the whole thing? :-(
+        if (!header) {
+            return callback(null, "Couldn't access BAI");
+        }
+
+        var uncba = new Uint8Array(header);
+        var baiMagic = readInt(uncba, 0);
+        if (baiMagic != BAI_MAGIC) {
+            return callback(null, 'Not a BAI file, magic=0x' + baiMagic.toString(16));
+        }
+
+        var nref = readInt(uncba, 4);
+
+        bam.indices = [];
+
+        var p = 8;
+        var minBlockIndex = 1000000000;
+        for (var ref = 0; ref < nref; ++ref) {
+            var blockStart = p;
+            var nbin = readInt(uncba, p); p += 4;
+            for (var b = 0; b < nbin; ++b) {
+                var bin = readInt(uncba, p);
+                var nchnk = readInt(uncba, p+4);
+                p += 8 + (nchnk * 16);
+            }
+            var nintv = readInt(uncba, p); p += 4;
+            
+            var q = p;
+            for (var i = 0; i < nintv; ++i) {
+                var v = readVob(uncba, q); q += 8;
+                if (v) {
+                    var bi = v.block;
+                    if (v.offset > 0)
+                        bi += 65536;
+
+                    if (bi < minBlockIndex)
+                        minBlockIndex = bi;
+                    break;
+                }
+            }
+            p += (nintv * 8);
+
+
+            if (nbin > 0) {
+                bam.indices[ref] = new Uint8Array(header, blockStart, p - blockStart);
+            }                     
+        }
+
+        bam.data.slice(0, minBlockIndex).fetch(function(r) {
+            if (!r) {
+                return callback(null, "Couldn't access BAM");
+            }
+            
+            var unc = unbgzf(r, r.byteLength);
+            var uncba = new Uint8Array(unc);
+
+            var magic = readInt(uncba, 0);
+            if (magic != BAM_MAGIC) {
+                return callback(null, "Not a BAM file, magic=0x" + magic.toString(16));
+            }
+            var headLen = readInt(uncba, 4);
+            var header = '';
+            for (var i = 0; i < headLen; ++i) {
+                header += String.fromCharCode(uncba[i + 8]);
+            }
+
+            var nRef = readInt(uncba, headLen + 8);
+            var p = headLen + 12;
+
+            bam.chrToIndex = {};
+            bam.indexToChr = [];
+            for (var i = 0; i < nRef; ++i) {
+                var lName = readInt(uncba, p);
+                var name = '';
+                for (var j = 0; j < lName-1; ++j) {
+                    name += String.fromCharCode(uncba[p + 4 + j]);
+                }
+                var lRef = readInt(uncba, p + lName + 4);
+                bam.chrToIndex[name] = i;
+                if (name.indexOf('chr') == 0) {
+                    bam.chrToIndex[name.substring(3)] = i;
+                } else {
+                    bam.chrToIndex['chr' + name] = i;
+                }
+                bam.indexToChr.push(name);
+
+                p = p + 8 + lName;
+            }
+
+            if (bam.indices) {
+                return callback(bam);
+            }
+        });
+    });
+}
+
+
+
+BamFile.prototype.blocksForRange = function(refId, min, max) {
+    var index = this.indices[refId];
+    if (!index) {
+        return [];
+    }
+
+    var intBinsL = reg2bins(min, max);
+    var intBins = [];
+    for (var i = 0; i < intBinsL.length; ++i) {
+        intBins[intBinsL[i]] = true;
+    }
+    var leafChunks = [], otherChunks = [];
+
+    var nbin = readInt(index, 0);
+    var p = 4;
+    for (var b = 0; b < nbin; ++b) {
+        var bin = readInt(index, p);
+        var nchnk = readInt(index, p+4);
+//        dlog('bin=' + bin + '; nchnk=' + nchnk);
+        p += 8;
+        if (intBins[bin]) {
+            for (var c = 0; c < nchnk; ++c) {
+                var cs = readVob(index, p);
+                var ce = readVob(index, p + 8);
+                (bin < 4681 ? otherChunks : leafChunks).push(new Chunk(cs, ce));
+                p += 16;
+            }
+        } else {
+            p +=  (nchnk * 16);
+        }
+    }
+//    dlog('leafChunks = ' + miniJSONify(leafChunks));
+//    dlog('otherChunks = ' + miniJSONify(otherChunks));
+
+    var nintv = readInt(index, p);
+    var lowest = null;
+    var minLin = Math.min(min>>14, nintv - 1), maxLin = Math.min(max>>14, nintv - 1);
+    for (var i = minLin; i <= maxLin; ++i) {
+        var lb =  readVob(index, p + 4 + (i * 8));
+        if (!lb) {
+            continue;
+        }
+        if (!lowest || lb.block < lowest.block || lb.offset < lowest.offset) {
+            lowest = lb;
+        }
+    }
+    // dlog('Lowest LB = ' + lowest);
+    
+    var prunedOtherChunks = [];
+    if (lowest != null) {
+        for (var i = 0; i < otherChunks.length; ++i) {
+            var chnk = otherChunks[i];
+            if (chnk.maxv.block >= lowest.block && chnk.maxv.offset >= lowest.offset) {
+                prunedOtherChunks.push(chnk);
+            }
+        }
+    }
+    // dlog('prunedOtherChunks = ' + miniJSONify(prunedOtherChunks));
+    otherChunks = prunedOtherChunks;
+
+    var intChunks = [];
+    for (var i = 0; i < otherChunks.length; ++i) {
+        intChunks.push(otherChunks[i]);
+    }
+    for (var i = 0; i < leafChunks.length; ++i) {
+        intChunks.push(leafChunks[i]);
+    }
+
+    intChunks.sort(function(c0, c1) {
+        var dif = c0.minv.block - c1.minv.block;
+        if (dif != 0) {
+            return dif;
+        } else {
+            return c0.minv.offset - c1.minv.offset;
+        }
+    });
+    var mergedChunks = [];
+    if (intChunks.length > 0) {
+        var cur = intChunks[0];
+        for (var i = 1; i < intChunks.length; ++i) {
+            var nc = intChunks[i];
+            if (nc.minv.block == cur.maxv.block /* && nc.minv.offset == cur.maxv.offset */) { // no point splitting mid-block
+                cur = new Chunk(cur.minv, nc.maxv);
+            } else {
+                mergedChunks.push(cur);
+                cur = nc;
+            }
+        }
+        mergedChunks.push(cur);
+    }
+    // dlog('mergedChunks = ' + miniJSONify(mergedChunks));
+
+    return mergedChunks;
+}
+
+BamFile.prototype.fetch = function(chr, min, max, callback, opts) {
+    var thisB = this;
+    opts = opts || {};
+
+    var chrId = this.chrToIndex[chr];
+    var chunks;
+    if (chrId === undefined) {
+        chunks = [];
+    } else {
+        chunks = this.blocksForRange(chrId, min, max);
+        if (!chunks) {
+            callback(null, 'Error in index fetch');
+        }
+    }
+    
+    var records = [];
+    var index = 0;
+    var data;
+
+    function tramp() {
+        if (index >= chunks.length) {
+            return callback(records);
+        } else if (!data) {
+            // dlog('fetching ' + index);
+            var c = chunks[index];
+            var fetchMin = c.minv.block;
+            var fetchMax = c.maxv.block + (1<<16); // *sigh*
+            thisB.data.slice(fetchMin, fetchMax - fetchMin).fetch(function(r) {
+                data = unbgzf(r, c.maxv.block - c.minv.block + 1);
+                return tramp();
+            });
+        } else {
+            var ba = new Uint8Array(data);
+            thisB.readBamRecords(ba, chunks[index].minv.offset, records, min, max, chrId, opts);
+            data = null;
+            ++index;
+            return tramp();
+        }
+    }
+    tramp();
+}
+
+var SEQRET_DECODER = ['=', 'A', 'C', 'x', 'G', 'x', 'x', 'x', 'T', 'x', 'x', 'x', 'x', 'x', 'x', 'N'];
+var CIGAR_DECODER = ['M', 'I', 'D', 'N', 'S', 'H', 'P', '=', 'X', '?', '?', '?', '?', '?', '?', '?'];
+
+function BamRecord() {
+}
+
+BamFile.prototype.readBamRecords = function(ba, offset, sink, min, max, chrId, opts) {
+    while (true) {
+        var blockSize = readInt(ba, offset);
+        var blockEnd = offset + blockSize + 4;
+        if (blockEnd >= ba.length) {
+            return sink;
+        }
+
+        var record = new BamRecord();
+
+        var refID = readInt(ba, offset + 4);
+        var pos = readInt(ba, offset + 8);
+        
+        var bmn = readInt(ba, offset + 12);
+        var bin = (bmn & 0xffff0000) >> 16;
+        var mq = (bmn & 0xff00) >> 8;
+        var nl = bmn & 0xff;
+
+        var flag_nc = readInt(ba, offset + 16);
+        var flag = (flag_nc & 0xffff0000) >> 16;
+        var nc = flag_nc & 0xffff;
+    
+        var lseq = readInt(ba, offset + 20);
+        
+        var nextRef  = readInt(ba, offset + 24);
+        var nextPos = readInt(ba, offset + 28);
+        
+        var tlen = readInt(ba, offset + 32);
+    
+        record.segment = this.indexToChr[refID];
+        record.flag = flag;
+        record.pos = pos;
+        record.mq = mq;
+        if (opts.light)
+            record.seqLength = lseq;
+
+        if (!opts.light) {
+            if (nextRef >= 0) {
+                record.nextSegment = this.indexToChr[nextRef];
+                record.nextPos = nextPos;
+            }
+
+            var readName = '';
+            for (var j = 0; j < nl-1; ++j) {
+                readName += String.fromCharCode(ba[offset + 36 + j]);
+            }
+            record.readName = readName;
+        
+            var p = offset + 36 + nl;
+
+            var cigar = '';
+            for (var c = 0; c < nc; ++c) {
+                var cigop = readInt(ba, p);
+                cigar = cigar + (cigop>>4) + CIGAR_DECODER[cigop & 0xf];
+                p += 4;
+            }
+            record.cigar = cigar;
+        
+            var seq = '';
+            var seqBytes = (lseq + 1) >> 1;
+            for (var j = 0; j < seqBytes; ++j) {
+                var sb = ba[p + j];
+                seq += SEQRET_DECODER[(sb & 0xf0) >> 4];
+                seq += SEQRET_DECODER[(sb & 0x0f)];
+            }
+            p += seqBytes;
+            record.seq = seq;
+
+            var qseq = '';
+            for (var j = 0; j < lseq; ++j) {
+                qseq += String.fromCharCode(ba[p + j] + 33);
+            }
+            p += lseq;
+            record.quals = qseq;
+
+            while (p < blockEnd) {
+                var tag = String.fromCharCode(ba[p], ba[p + 1]);
+                var type = String.fromCharCode(ba[p + 2]);
+                var value;
+
+                if (type == 'A') {
+                    value = String.fromCharCode(ba[p + 3]);
+                    p += 4;
+                } else if (type == 'i' || type == 'I') {
+                    value = readInt(ba, p + 3);
+                    p += 7;
+                } else if (type == 'c' || type == 'C') {
+                    value = ba[p + 3];
+                    p += 4;
+                } else if (type == 's' || type == 'S') {
+                    value = readShort(ba, p + 3);
+                    p += 5;
+                } else if (type == 'f') {
+                    value = readFloat(ba, p + 3);
+                    p += 7;
+                } else if (type == 'Z' || type == 'H') {
+                    p += 3;
+                    value = '';
+                    for (;;) {
+                        var cc = ba[p++];
+                        if (cc == 0) {
+                            break;
+                        } else {
+                            value += String.fromCharCode(cc);
+                        }
+                    }
+                } else if (type == 'B') {
+                    var atype = String.fromCharCode(ba[p + 3]);
+                    var alen = readInt(ba, p + 4);
+                    var elen;
+                    var reader;
+                    if (atype == 'i' || atype == 'I' || atype == 'f') {
+                        elen = 4;
+                        if (atype == 'f')
+                            reader = readFloat;
+                        else
+                            reader = readInt;
+                    } else if (atype == 's' || atype == 'S') {
+                        elen = 2;
+                        reader = readShort;
+                    } else if (atype == 'c' || atype == 'C') {
+                        elen = 1;
+                        reader = readByte;
+                    } else {
+                        throw 'Unknown array type ' + atype;
+                    }
+
+                    p += 8;
+                    value = [];
+                    for (var i = 0; i < alen; ++i) {
+                        value.push(reader(ba, p));
+                        p += elen;
+                    }
+                } else {
+                    throw 'Unknown type '+ type;
+                }
+                record[tag] = value;
+            }
+        }
+
+        if (!min || record.pos <= max && record.pos + lseq >= min) {
+            if (chrId === undefined || refID == chrId) {
+                sink.push(record);
+            }
+        }
+        offset = blockEnd;
+    }
+
+    // Exits via top of loop.
+};
+
+if (typeof(module) !== 'undefined') {
+    module.exports = {
+        makeBam: makeBam,
+        BAM_MAGIC: BAM_MAGIC,
+        BAI_MAGIC: BAI_MAGIC,
+        BamFlags: BamFlags
+    };
+}
+/* jshint ignore:end */
---
+++ htdocs/core/dalliance/js/sample.js
@@ -0,0 +1,149 @@
+/* jshint ignore:start */
+/* -*- mode: javascript; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+// 
+// Dalliance Genome Explorer
+// (c) Thomas Down 2006-2010
+//
+// sample.js: downsampling of quantitative features
+//
+
+"use strict";
+
+if (typeof(require) !== 'undefined') {
+    var das = require('./das');
+    var DASFeature = das.DASFeature;
+}
+
+var __DS_SCALES = [1, 2, 5];
+
+function ds_scale(n) {
+    return __DS_SCALES[n % __DS_SCALES.length] * Math.pow(10, (n / __DS_SCALES.length)|0);
+}
+
+
+function DSBin(scale, min, max) {
+    this.scale = scale;
+    this.tot = 0;
+    this.cnt = 0;
+    this.hasScore = false;
+    this.min = min; this.max = max;
+    this.features = [];
+}
+
+function _featureOrder(a, b) {
+    if (a.min < b.min) {
+        return -1;
+    } else if (a.min > b.min) {
+        return 1;
+    } else if (a.max < b.max) {
+        return -1;
+    } else if (b.max > a.max) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+DSBin.prototype.score = function() {
+    if (this.cnt == 0) {
+        return 0;
+    } else if (this.hasScore) {
+        return this.tot / this.cnt;
+    } else {
+        var features = this.features;
+        features.sort(_featureOrder);
+
+        var maxSeen = -10000000000;
+        var cov=0, lap=0;
+
+        for (var fi = 1; fi < features.length; ++fi) {
+            var f = features[fi];
+            var lMin = Math.max(f.min, this.min);
+            var lMax = Math.min(f.max, this.max);
+            lap += (lMax - lMin + 1);
+
+            if (lMin > maxSeen) {
+                cov += lMax - lMin + 1;
+                maxSeen = lMax;
+            } else {
+                if (lMax > maxSeen) {
+                    cov += (lMax - maxSeen);
+                    maxSeen = lMax;
+                }
+            }
+        }
+
+        if (cov > 0)
+            return (1.0 * lap) / cov;
+        else
+            return 0;
+    }
+}
+
+DSBin.prototype.feature = function(f) {
+    if (f.score !== undefined) {
+        this.tot += f.score;
+        this.hasScore = true
+    }
+
+    ++this.cnt;
+    this.features.push(f);
+}
+
+function downsample(features, targetRez) {
+    var sn = 0;
+    while (ds_scale(sn + 1) < targetRez) {
+        ++sn;
+    }
+    var scale = ds_scale(sn);
+
+    var binTots = [];
+    var maxBin = -10000000000;
+    var minBin = 10000000000;
+    for (var fi = 0; fi < features.length; ++fi) {
+        var f = features[fi];
+        if (f.groups && f.groups.length > 0) {
+            // Don't downsample complex features (?)
+            return features;
+        }
+
+        var minLap = (f.min / scale)|0;
+        var maxLap = (f.max / scale)|0;
+        maxBin = Math.max(maxBin, maxLap);
+        minBin = Math.min(minBin, minLap);
+        for (var b = minLap; b <= maxLap; ++b) {
+            var bm = binTots[b];
+            if (!bm) {
+                bm = new DSBin(scale, b * scale, (b + 1) * scale - 1);
+                binTots[b] = bm;
+            }
+            bm.feature(f);
+        }
+    }
+
+    var sampledFeatures = [];
+    for (var b = minBin; b <= maxBin; ++b) {
+        var bm = binTots[b];
+        if (bm) {
+            var f = new DASFeature();
+            f.segment = features[0].segment;
+            f.min = (b * scale) + 1;
+            f.max = (b + 1) * scale;
+            f.score = bm.score();
+            f.type = 'density';
+            sampledFeatures.push(f);
+        }
+    }
+
+    var afterDS = Date.now();
+    return sampledFeatures;
+}
+
+if (typeof(module) !== 'undefined') {
+    module.exports = {
+        downsample: downsample
+    };
+}
+
+/* jshint ignore:end */
